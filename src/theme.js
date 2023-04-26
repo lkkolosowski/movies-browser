ad5fa7e64a37732354c989c16e112447573fa343
Albrecht-Albi <albrecht.bar1@gmail.com>
2023-04-26 10:01:51 +0200

    adding colors and screen resolution points

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  color: {
+    whisper: "#F5F5FA",
+    white: "#ffffff",
+    lightBlue: "#D6E4FF",
+    active: "#0044CC",
+    grey: "E4E6F0",
+    darkerGrey: "#7E839A",
+    stormGrey: "#74788B",
+    inactive: "#7E839A",
+    waterloo: "#7E839A",
+    woodsmoke: "#18181B",
+    black: "#000000"
+
+
+  }
+}
