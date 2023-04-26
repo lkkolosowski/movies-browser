64333610de5d4cc6c1186ce1cc4fb12227f2da8f
Albrecht-Albi <albrecht.bar1@gmail.com>
2023-04-26 13:38:55 +0200

    correction of the path and content of the container

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,7 +1,7 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-export const Container = styled.main`
-margin: 0 auto;
-max-width: 1368px;
-padding: 8px;
+export const Container = styled.div`
+  padding: 16px;
+  max-width: 1216px;
+  margin: 100px auto;
 `;
