cc3089f73e5571917fc25546c37615b7a96a3fc5
PawelDzierzbicki <ativek23@gmail.com>
2023-04-26 18:16:52 +0200

    deleted unnecessary lines

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { Container } from "./Container";
 import { Navigation } from "./common/Navigation";
-=======
-import { Container } from "./common/Container/styled";
->>>>>>> 64333610de5d4cc6c1186ce1cc4fb12227f2da8f
 
 function App() {
   return (
