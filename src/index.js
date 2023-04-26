2320bcb28789ff113ab0c36071f87acf39bcc52e
lkkolosowski <lkkolosowski@gmail.com>
2023-04-26 20:25:07 +0200

    add GlobalStyle and Normalize to index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
+import { Normalize } from "styled-normalize";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <App />
+      <GlobalStyle />
+      <Normalize />
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
