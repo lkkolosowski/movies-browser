28bd3a554fabffafcfcc49fa2b1f79cd769efc6b
lkkolosowski <lkkolosowski@gmail.com>
2023-04-27 21:03:55 +0200

    created Tiles component

---
+++ src/common/Tiles/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledTiles = styled.ul`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+margin: 0;
+padding: 0;
+list-style-type: none;
+`;
