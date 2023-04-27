8a7516be1bbfafe53009b85d2a8565b83e2a73d7
lkkolosowski <lkkolosowski@gmail.com>
2023-04-27 19:11:10 +0200

    created popularMovies reducer

--- src/store.js
+++ src/store.js
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./rootSaga";
+import popularMoviesReducer from "./features/popularMoviesSlice";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    popularMovies: popularMoviesReducer,
+  },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
