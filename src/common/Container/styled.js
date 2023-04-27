78ac79aa8c815c00cb152a67d655886d50a3b7d2
lkkolosowski <lkkolosowski@gmail.com>
2023-04-27 21:00:54 +0200

    added styles to Container

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  padding: 16px;
-  max-width: 1216px;
-  margin: 100px auto;
+export const Container = styled.main`
+  max-width: 1368px;
+  margin: 56px auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.full}) {
+    padding: 0 16px;
+  }
+
 `;
