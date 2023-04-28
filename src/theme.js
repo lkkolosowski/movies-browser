085b8b63325a9ed9a33eaf93da3022dec0a34fde
Albrecht-Albi <albrecht.bar1@gmail.com>
2023-04-28 19:51:09 +0200

    add shade

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,11 @@
 export const theme = {
   color: {
     whisper: "#F5F5FA",
-    white: "#ffffff",
+    white: "#FFFFFF",
     pattensBlue: "#D6E4FF",
     scienceBlue: "#0044CC",
-    mystic: "#E4E6F0",
+    mine: "#333333",
+    mystic: "E4E6F0",
     mercury: "#E5E5E5",
     lynch: "#627489",
     snuff: "#DDDDEE",
@@ -21,4 +22,6 @@ export const theme = {
     small: "767px",
     mobile: "550px",
   },
+
+  shadow: "rgba(186, 199, 213, 0.5)",
 };
