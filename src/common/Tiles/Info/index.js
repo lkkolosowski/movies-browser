8f39b6584db292d5294eadf1b80debcc5b9bfee9
lkkolosowski <lkkolosowski@gmail.com>
2023-05-01 02:18:24 +0200

    created Info component for movies details

--- src/common/Tiles/Info/index.js
+++ src/common/Tiles/Info/index.js
@@ -1,9 +1,46 @@
-import { Title, Subtitle } from "./styled";
-import { extractFirstWord } from "./utils";
+import {
+  Title,
+  Subtitle,
+  Wrapper,
+  Paragraph,
+  Attribute,
+  Value,
+  TitleDetails,
+  SubtitleDetails,
+} from "./styled";
+import {
+  extractFirstWord,
+  separateNamesByCommas,
+  reverseString,
+} from "./utils";
 
 export const MainInfo = ({ title, year }) => (
   <div>
     <Title>{title}</Title>
-    <Subtitle>{extractFirstWord(year)}</Subtitle>
+    {year && <Subtitle>{extractFirstWord(year)}</Subtitle>}
   </div>
+);
+
+export const AdditionalInfo = ({ production, release }) => (
+  <Wrapper>
+    {production.length !== 0 && (
+      <Paragraph>
+        <Attribute mobile>Production:</Attribute>
+        <Value>{separateNamesByCommas(production)}</Value>
+      </Paragraph>
+    )}
+    {release && (
+      <Paragraph>
+        <Attribute mobile>Release date:</Attribute>
+        <Value>{reverseString(release)}</Value>
+      </Paragraph>
+    )}
+  </Wrapper>
+);
+
+export const MainInfoDetails = ({ title, year }) => (
+  <>
+    <TitleDetails>{title}</TitleDetails>
+    {year && <SubtitleDetails>{extractFirstWord(year)}</SubtitleDetails>}
+  </>
 );
--- src/common/Tiles/Info/styled.js
+++ src/common/Tiles/Info/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Title = styled.h2`
   margin: 16px 0 8px;
@@ -29,5 +29,72 @@ export const Subtitle = styled.span`
 
   @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
     font-size: 13px;
+  }
+`;
+
+export const TitleDetails = styled.h2`
+  margin: 8px 0 24px;
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 1.2;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
+    margin: 0 0 8px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    font-weight: 500;
+    line-height: 1.3;
+    font-size: 16px;
+    margin: 0 0 4px;
+  }
+`;
+
+export const SubtitleDetails = styled.span`
+  font-size: 22px;
+  font-weight: 400;
+  line-height: 1.2;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    line-height: 1.3;
+    font-size: 13px;
+    color: ${({ theme }) => theme.color.waterloo};
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin: 24px 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
+    margin: 8px 0;
+  }
+`;
+
+export const Paragraph = styled.div`
+  line-height: 1.2;
+  display: inline-flex;
+  font-size: 18px;
+  gap: 10px;
+`;
+
+export const Attribute = styled.span`
+  color: ${({ theme }) => theme.color.stormGrey};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    ${({ mobile }) =>
+      mobile &&
+      css`
+        display: none;
+      `}
+  }
+`;
+
+export const Value = styled.span`
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    line-height: 1.3;
+    font-size: 12px;
   }
 `;
