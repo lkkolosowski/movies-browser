87c8d51711e52293e096c14b930699b3d670f401
lkkolosowski <lkkolosowski@gmail.com>
2023-05-01 02:19:47 +0200

    created Tile component for movies details

--- src/common/Tiles/index.js
+++ src/common/Tiles/index.js
@@ -1,16 +1,54 @@
-import { Genres } from "./Genres";
-import { MovieImage } from "./Images";
-import { MainInfo } from "./Info";
-import { Rating } from "./Rating";
-import { Description, Tile } from "./styled";
+import { Genres, GenresDetails } from "./Genres";
+import { MovieImage, MovieDetailsImage } from "./Images";
+import { AdditionalInfo, MainInfo, MainInfoDetails } from "./Info";
+import { DetailsRating, Rating } from "./Rating";
+import {
+  MovieDescription,
+  MovieDetailsDescription,
+  Movie,
+  Overview,
+  MovieDetails,
+} from "./styled";
 
-export const MovieTile = ({ poster, title, year, vote, votes, genres }) => (
-  <Tile>
-    <MovieImage poster={poster} />
-    <Description>
+export const MovieTile = ({
+  poster,
+  title,
+  year,
+  vote,
+  votes,
+  genres,
+  size,
+}) => (
+  <Movie>
+    <MovieImage poster={poster} size={size} />
+    <MovieDescription>
       <MainInfo title={title} year={year} />
       <Genres genres={genres} />
       <Rating vote={vote} votes={votes} />
-    </Description>
-  </Tile>
+    </MovieDescription>
+  </Movie>
+);
+
+export const MovieDetailsTile = ({
+  poster,
+  title,
+  year,
+  genres,
+  vote,
+  votes,
+  overview,
+  production,
+  release,
+  size,
+}) => (
+  <MovieDetails>
+    <MovieDetailsImage poster={poster} size={size} />
+    <MovieDetailsDescription>
+      <MainInfoDetails title={title} year={year} />
+      <AdditionalInfo production={production} release={release} />
+      <GenresDetails genres={genres} />
+      <DetailsRating vote={vote} votes={votes} />
+    </MovieDetailsDescription>
+    <Overview>{overview}</Overview>
+  </MovieDetails>
 );
--- src/common/Tiles/styled.js
+++ src/common/Tiles/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Tile = styled.article`
+export const Movie = styled.article`
   display: grid;
   grid-template-columns: 1fr;
   padding: 16px;
@@ -13,8 +13,32 @@ export const Tile = styled.article`
   }
 `;
 
-export const Description = styled.div`
+export const MovieDetails = styled.article`
+  color: ${({ theme }) => theme.color.black};
+  margin: 56px 0;
+  padding: 40px;
+  display: grid;
+  gap: 0 40px;
+  grid-template-columns: minmax(auto, 312px) 1fr;
+  background-color: ${({ theme }) => theme.color.white};
+  box-shadow: ${({ theme }) => theme.shadow};
+  grid-template-rows: auto 1fr;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    gap: 0 28px;
+    grid-template-columns: minmax(auto, 215px) 1fr;
+    padding: 28px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    grid-template-columns: minmax(auto, 114px) 1fr;
+    gap: 0 16px;
+    padding: 16px;
+  }
+`;
+
+export const MovieDescription = styled.div`
   height: 272px;
+  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -22,5 +46,30 @@ export const Description = styled.div`
   @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
     height: auto;
     justify-content: flex-start;
+  }
+`;
+
+export const MovieDetailsDescription = styled.div`
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    grid-row: span 2;
+  }
+`;
+
+export const Overview = styled.p`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 1.6;
+  color: ${({ theme }) => theme.color.black};
+  margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    font-size: 17px;
+    grid-row: 3;
+    grid-column: span 2;
+    margin-top: 24px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    font-size: 14px;
+    margin-top: 16px;
   }
 `;
