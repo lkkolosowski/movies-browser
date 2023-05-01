99a73448bebd21aa5613371efb2b561254957328
lkkolosowski <lkkolosowski@gmail.com>
2023-05-01 02:18:57 +0200

    added styles for genres for movies details

--- src/common/Tiles/Genres/styled.js
+++ src/common/Tiles/Genres/styled.js
@@ -1,11 +1,16 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  padding: 0px;
   gap: 8px;
-  margin: 8px 0 16px;
+  margin: 8px 0;
+
+  ${({ details }) =>
+    details &&
+    css`
+      margin: 0;
+    `}
 `;
 
 export const Genre = styled.p`
@@ -15,9 +20,23 @@ export const Genre = styled.p`
   padding: 8px 16px;
   font-size: 14px;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-    align-self: flex-start;
-    font-size: 10px;
-    padding: 4px 8px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    ${({ details }) =>
+      !details &&
+      css`
+        align-self: flex-start;
+        font-size: 10px;
+        padding: 4px 8px;
+      `}
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
+    ${({ details }) =>
+      details &&
+      css`
+        align-self: flex-start;
+        font-size: 10px;
+        padding: 4px 8px;
+      `}
   }
 `;
