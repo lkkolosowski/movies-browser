d27a8eff3bc90b7c483922f1874de9df02a0be1b
lkkolosowski <lkkolosowski@gmail.com>
2023-05-04 21:43:50 +0200

    created Subtitle component

--- src/common/Title/styled.js
+++ src/common/Title/styled.js
@@ -12,3 +12,16 @@ export const StyledTitle = styled.h1`
     margin: 0 0 12px;
   }
 `;
+
+export const StyledSubtitle = styled.h2`
+  font-size: 36px;
+  font-weight: 600;
+  margin-top: 64px;
+  margin-bottom: 32px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    font-size: 20px;
+    margin-top: 24px;
+    margin-bottom: 16px;
+  }
+`;
