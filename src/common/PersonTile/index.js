724d114866fd10e99a508d36ab35e23cbded689c
lkkolosowski <lkkolosowski@gmail.com>
2023-05-09 01:57:34 +0200

    added link to person in person tile

--- src/common/PersonTile/index.js
+++ src/common/PersonTile/index.js
@@ -1,10 +1,13 @@
+import { toPerson } from "../../routes";
 import { PersonImage } from "../Images";
-import { Name, Person, Role } from "./styled";
+import { Name, Person, Role, StyledLink } from "./styled";
 
 export const PersonTile = ({ name, role, poster, id }) => (
-  <Person>
-    <PersonImage poster={poster} id={id}/>
-    <Name>{name}</Name>
-    <Role>{role}</Role>
-  </Person>
+  <StyledLink to={toPerson({ id: id })}>
+    <Person>
+      <PersonImage poster={poster} id={id} />
+      <Name>{name}</Name>
+      <Role>{role}</Role>
+    </Person>
+  </StyledLink>
 );
