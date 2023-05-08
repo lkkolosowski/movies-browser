37a3c747675594632c550673fadd2eb88feafc17
lkkolosowski <lkkolosowski@gmail.com>
2023-05-08 23:08:07 +0200

    moved pagination to main wrapper (padding fix)

--- src/features/MoviesList/index.js
+++ src/features/MoviesList/index.js
@@ -1,5 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
-import { goToPage, selectMoviesList, selectPage, selectStatus, selectTotalPages } from "../moviesListSlice";
+import {
+  goToPage,
+  selectMoviesList,
+  selectPage,
+  selectStatus,
+  selectTotalPages,
+} from "../moviesListSlice";
 import { MainWrapper } from "../../common/MainWrapper";
 import { MovieTile } from "../../common/MovieTile";
 import { Pagination } from "../../common/Pagination";
@@ -27,38 +33,36 @@ const MoviesList = () => {
     <Loader /> :
     status === "error" ?
     <Error /> :
-    <>
-      <MainWrapper
-        content={
-          <>
-            <Title title="Popular Movies" />
-            <List>
-              {popularMovies.map((movie) => (
-                <Item key={movie.id}>
-                  <MovieTile
-                    id={movie.id}
-                    poster={movie.poster_path}
-                    title={movie.title}
-                    year={movie.release_date}
-                    vote={movie.vote_average}
-                    votes={movie.vote_count}
-                    genres={movie.genre_ids}
-                  />
-                </Item>
-              ))}
-            </List>
-          </>
-        }
-      />
-      <Pagination
-        toNextPage={() => dispatch(goToPage({ page: pageNumber + 1 }))}
-        toPrevPage={() => dispatch(goToPage({ page: pageNumber - 1 }))}
-        pageNumber={pageNumber}
-        totalPages={totalPages}
-        toFirstPage={() => dispatch(goToPage({ page: 1 }))}
-        toLastPage={() => dispatch(goToPage({ page: totalPages }))}
-      />
-    </>
+    <MainWrapper
+      content={
+        <>
+          <Title title="Popular Movies" />
+          <List>
+            {popularMovies.map((movie) => (
+              <Item key={movie.id}>
+                <MovieTile
+                  id={movie.id}
+                  poster={movie.poster_path}
+                  title={movie.title}
+                  year={movie.release_date}
+                  vote={movie.vote_average}
+                  votes={movie.vote_count}
+                  genres={movie.genre_ids}
+                />
+              </Item>
+            ))}
+          </List>
+          <Pagination
+            toNextPage={() => dispatch(goToPage({ page: pageNumber + 1 }))}
+            toPrevPage={() => dispatch(goToPage({ page: pageNumber - 1 }))}
+            pageNumber={pageNumber}
+            totalPages={totalPages}
+            toFirstPage={() => dispatch(goToPage({ page: 1 }))}
+            toLastPage={() => dispatch(goToPage({ page: totalPages }))}
+          />
+        </>
+      }
+    />
   );
 };
 
