e0440bf39e27042e798abeb41b46d72d7b664c0f
lkkolosowski <lkkolosowski@gmail.com>
2023-05-09 01:58:05 +0200

    added specified genres person tile

--- src/common/Genres/index.js
+++ src/common/Genres/index.js
@@ -1,7 +1,8 @@
 import { useSelector } from "react-redux";
 import { Wrapper, Genre } from "./styled";
-import { selectGenres } from "../../features/moviesListSlice";
 import { getNames, extractNames } from "./utils";
+import { selectPersonGenres } from "../../features/personSlice";
+import { selectGenres } from "../../features/moviesListSlice";
 
 export const Genres = ({ genres }) => {
   const genresId = useSelector(selectGenres);
@@ -23,6 +24,20 @@ export const GenresDetails = ({ genres }) => {
       <Wrapper details>
         {extractNames(genres).map((genre) => (
           <Genre details key={genre}>{genre}</Genre>
+        ))}
+      </Wrapper>
+    )
+  );
+};
+
+export const GenresPerson = ({ genres }) => {
+  const genresId = useSelector(selectPersonGenres);
+
+  return (
+    genres && (
+      <Wrapper>
+        {getNames(genres, genresId).map((genre) => (
+          <Genre key={genre}>{genre}</Genre>
         ))}
       </Wrapper>
     )
