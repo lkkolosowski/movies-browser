43b0900f7bef73367d68fe6c07190449d4bac9eb
lkkolosowski <lkkolosowski@gmail.com>
2023-05-09 02:01:36 +0200

    created specified movie tile for pesron

--- src/common/MovieTile/index.js
+++ src/common/MovieTile/index.js
@@ -1,11 +1,8 @@
-import { Genres } from "../Genres";
+import { Genres, GenresPerson } from "../Genres";
 import { MovieImage } from "../Images";
 import { MainInfo } from "../Info";
 import { Rating } from "../Rating";
-import {
-  MovieDescription,
-  Movie,
-} from "./styled";
+import { MovieDescription, Movie } from "./styled";
 
 export const MovieTile = ({
   poster,
@@ -22,6 +19,29 @@ export const MovieTile = ({
       <div>
         <MainInfo title={title} year={year} />
         <Genres genres={genres} />
+      </div>
+      <div>
+        <Rating vote={vote} votes={votes} />
+      </div>
+    </MovieDescription>
+  </Movie>
+);
+
+export const MoviePersonTile = ({
+  poster,
+  title,
+  year,
+  vote,
+  votes,
+  genres,
+  id,
+}) => (
+  <Movie>
+    <MovieImage id={id} poster={poster} />
+    <MovieDescription>
+      <div>
+        <MainInfo title={title} year={year} />
+        <GenresPerson genres={genres} />
       </div>
       <div>
         <Rating vote={vote} votes={votes} />
