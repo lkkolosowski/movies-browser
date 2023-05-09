77efb8ca3d27919906aed0174e1197e50172bf62
PawelDzierzbicki <ativek23@gmail.com>
2023-05-09 20:06:41 +0200

    adding SearchEngine

---
+++ src/common/Header/Search/index.js
@@ -0,0 +1,36 @@
+//import { useState } from "react";
+import { useLocation } from "react-router-dom";
+//import { toMovie, toPeople } from "../../../routes";
+import { Input } from "../styled";
+import { useQueryParameter, useReplaceQueryParameter } from "../../../features/queryParameters";
+import { searchQueryParamName} from "../../../features/queryParametersName";
+
+
+const SearchEngine = () => {
+
+    const replaceQueryParameter = useReplaceQueryParameter();
+    const query = useQueryParameter(searchQueryParamName);
+    const location = useLocation();
+
+    const inputChange = ({ target }) => {
+        replaceQueryParameter([
+            {
+                key: searchQueryParamName,
+                value: target.value.trim() !== "" ? target.value : undefined,
+            },
+        ]);
+    };
+
+    return (
+        <Input
+            value={query || ""}
+            onChange={inputChange}
+            placeholder={`${location.pathname.includes("movie")
+                ? "Search for movies..."
+                : "Search for people..."
+                }`}
+        />
+    );
+};
+
+export default SearchEngine;
