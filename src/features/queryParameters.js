24766ac26dedb6c57dee71e4a68a4bbecc67bf6d
lkkolosowski <lkkolosowski@gmail.com>
2023-05-11 17:20:08 +0200

    formatted code

--- src/features/queryParameters.js
+++ src/features/queryParameters.js
@@ -1,27 +1,27 @@
 import { useLocation, useHistory } from "react-router-dom";
 
 export const useQueryParameter = (key) => {
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-    return searchParams.get(key);
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  return searchParams.get(key);
 };
 
 export const useReplaceQueryParameter = () => {
-    const location = useLocation();
-    const history = useHistory();
+  const location = useLocation();
+  const history = useHistory();
 
-    const replaceQueryParameter = (keyValue) => {
-        const searchParams = new URLSearchParams(location.search);
+  const replaceQueryParameter = (keyValue) => {
+    const searchParams = new URLSearchParams(location.search);
 
-        for (const { key, value } of keyValue) {
-            if (value === undefined) {
-                searchParams.delete(key);
-            } else {
-                searchParams.set(key, value);
-            }
-        }
-        history.push(`${location.pathname}?${searchParams.toString()}`);
-    };
+    for (const { key, value } of keyValue) {
+      if (value === undefined) {
+        searchParams.delete(key);
+      } else {
+        searchParams.set(key, value);
+      }
+    }
+    history.push(`${location.pathname}?${searchParams.toString()}`);
+  };
 
-    return replaceQueryParameter;
+  return replaceQueryParameter;
 };
