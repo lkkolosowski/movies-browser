73001e63e9ddeee1b503e989af15b8817ebf3946
lkkolosowski <lkkolosowski@gmail.com>
2023-05-11 15:22:44 +0200

    implement fetching data of popular people to app

--- src/features/PeopleList/index.js
+++ src/features/PeopleList/index.js
@@ -1,8 +1,86 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  goToPage,
+  selectPeople,
+  selectPage,
+  selectStatus,
+  selectTotalPages,
+  selectTotalResults,
+  setQuery,
+} from "../peopleListSlice";
 import { MainWrapper } from "../../common/MainWrapper";
+import { Pagination } from "../../common/Pagination";
+import { Error } from "../../common/Error";
+import { Loader } from "../../common/Loader";
+import { NoResults } from "../../common/NoResults";
 import { Title } from "../../common/Title";
+import { List, Item } from "./styled";
+import { useQueryParameter } from "../queryParameters";
+import { PersonListTile } from "../../common/PersonTile";
+import {
+  pageQueryParamName,
+  searchQueryParamName,
+} from "../queryParametersName";
+import { useEffect } from "react";
 
-const PeopleList = () => (
-  <MainWrapper content={<Title title="Popular People"></Title>} />
-);
+const PeopleList = () => {
+  const popularPeople = useSelector(selectPeople);
+  const status = useSelector(selectStatus);
+
+  const pageNumber = useSelector(selectPage);
+  const totalPages = useSelector(selectTotalPages);
+
+  const query = useQueryParameter(searchQueryParamName);
+  const page = useQueryParameter(pageQueryParamName);
+  const totalResults = useSelector(selectTotalResults);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setQuery(query
+      ? { query: query }
+      : { query: "" }));
+    dispatch(goToPage(page
+      ? { page: page }
+      : { page: 1 }));
+  }, [query, page, dispatch]);
+
+  return (
+    status === "loading" ?
+    <Loader /> :
+    status === "error" ?
+    <Error /> :
+    <>
+      {totalResults === 0 ?
+        <NoResults /> :
+        <MainWrapper
+          content={
+            <>
+              <Title
+                title={
+                  query
+                    ? `Search results for "${query}" (${totalResults})`
+                    : `Popular People`
+                }
+              ></Title>
+              <List>
+                {popularPeople.map((person) => (
+                  <Item key={person.id}>
+                    <PersonListTile
+                      id={person.id}
+                      poster={person.profile_path}
+                      name={person.name}
+                    />
+                  </Item>
+                ))}
+              </List>
+              <Pagination pageNumber={pageNumber} totalPages={totalPages} />
+            </>
+          }
+        />
+      }
+    </>
+  );
+};
 
 export default PeopleList;
