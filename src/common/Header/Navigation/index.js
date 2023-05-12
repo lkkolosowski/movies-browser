30855392e1270510c5f9539abaa18b00764c6802
lkkolosowski <lkkolosowski@gmail.com>
2023-05-12 14:46:01 +0200

    added routes to navigation

--- src/common/Header/Navigation/index.js
+++ src/common/Header/Navigation/index.js
@@ -1,14 +1,15 @@
+import { toMovies, toPeople } from "../../../routes";
 import { StyledNavigation, Item, StyledNavLink } from "./styled";
 
 const Navigation = () => (
-    <StyledNavigation>
-      <Item>
-        <StyledNavLink to="/movies">MOVIES</StyledNavLink>
-      </Item>
-      <Item>
-        <StyledNavLink to="/people">PEOPLE</StyledNavLink>
-      </Item>
-    </StyledNavigation>
+  <StyledNavigation>
+    <Item>
+      <StyledNavLink to={toMovies()}>MOVIES</StyledNavLink>
+    </Item>
+    <Item>
+      <StyledNavLink to={toPeople()}>PEOPLE</StyledNavLink>
+    </Item>
+  </StyledNavigation>
 );
 
 export default Navigation;
