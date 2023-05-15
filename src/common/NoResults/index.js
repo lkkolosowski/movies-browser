663c8cf6bf8e6fe17b7aef940d8d95649ff501f7
PawelDzierzbicki <ativek23@gmail.com>
2023-05-15 15:43:35 +0200

    adding a title to noResult

--- src/common/NoResults/index.js
+++ src/common/NoResults/index.js
@@ -1,15 +1,22 @@
-import { StyledIcon, Wrapper } from "./styled";
+import { StyledIcon, Wrapper, Title } from "./styled";
 import { MainWrapper } from "../MainWrapper"
+import { useQueryParameter } from "../../features/queryParameters";
+import { searchQueryParamName } from "../../features/queryParametersName";
 
-export const NoResults = () => (
-  <MainWrapper
-    content={
-      <Wrapper>
-        <StyledIcon />
-      </Wrapper>
-    }
-    title={
-      `Sorry, there are no results for`
-    }
-  />
-);
+export const NoResults = () => {
+
+  const query = useQueryParameter(searchQueryParamName);
+
+  return (
+    <MainWrapper
+      content={
+        <Wrapper>
+          <Title>
+            Sorry, there are no results for "{query}"
+          </Title>
+          <StyledIcon />
+        </Wrapper>
+      }
+    />
+  );
+};
